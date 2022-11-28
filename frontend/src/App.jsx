96a8e363ca7bb8c003d59541ef6eb1eaafc30cee
Elef-TQ <tariq75aitlakhlift@gmail.com>
2022-11-28 17:03:41 +0000

    add footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer' ;
 
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Footer/>
     </div>
   );
 }
---
+++ frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Footer.css"
+const Footer = () => {
+  return (
+    <div className="footer-clean">
+    <footer>
+        <div className="container ">
+            <div className="row justify-content-center">
+                <div className="col-sm-4 col-md-3 item">
+                    <h3>Services</h3>
+                    <ul>
+                        <li><a href="#">Web design</a></li>
+                        <li><a href="#">Development</a></li>
+                        <li><a href="#">Hosting</a></li>
+                    </ul>
+                </div>
+                <div className="col-sm-4 col-md-3 item">
+                    <h3>About</h3>
+                    <ul>
+                        <li><a href="#">Jeea</a></li>
+                        <li><a href="#">Team</a></li>
+                        <li><a href="#">Legacy</a></li>
+                    </ul>
+                </div>
+                <div className="col-sm-4 col-md-3 item">
+                    <h3>Careers</h3>
+                    <ul>
+                        <li><a href="#">Job openings</a></li>
+                        <li><a href="#">Employee success</a></li>
+                        <li><a href="#">Benefits</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 item social">
+                    <a href="#"><i className="fa fa-github"></i></a>
+                    <a href="#"><i className="fa fa-linkedin"></i></a>
+                    <a href="#"><i className="fa fa-facebook "></i></a>
+                    <a href="#"><i className="fa fa-twitter "></i></a>
+                    <a href="#"><i className="fa fa-instagram"></i></a>
+                    <p className="copyright"><b>ENSA AGADIR : </b> JEEA © 2022</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>);
+};
+export default Footer;
