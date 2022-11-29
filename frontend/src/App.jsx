dd3b359b507672a74144f283e260facea5de7dc2
Elef-TQ <tariq75aitlakhlift@gmail.com>
2022-11-29 00:30:36 +0000

    add contact component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer' ;
+import Contact from './components/Contact/Contact' ;
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Footer/>
+      
+     
     </div>
   );
 }
