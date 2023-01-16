491c9443e1b32c388460fb355d590e9638bf17b9
Medmly20208 <moulaymohamed856@gmail.com>
2023-01-16 20:32:15 +0100

    add design for section3 section4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,26 @@
 import './App.css';
+import { useEffect } from 'react';
+
+// aos : animate on scroll package
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+
+//Components
+import JeeaPresentation from './components/jeeapresentation';
+
 
 function App() {
+   useEffect(()=>{
+    AOS.init();
+   },[])
+
   return (
     <div>
       <h1>
         JEEA
       </h1>
+      <JeeaPresentation/>
     </div>
   );
 }
