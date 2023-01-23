e6dc29564c1734c9f9ac212d73ccf55adcec1e3e
Medmly20208 <moulaymohamed856@gmail.com>
2023-01-23 16:49:38 +0100

    v1 of jeea section and cjem

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,31 @@
-<<<<<<< HEAD:frontend/src/App.js
-import './App.scss';
-=======
-import './App.css';
-import { useEffect } from 'react';
+import "./App.scss";
+import { useEffect } from "react";
 
 // aos : animate on scroll package
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 //Components
-import JeeaPresentation from './components/jeeapresentation';
->>>>>>> 369c28e2051fa4540339639b789099704c8ee4b3:frontend/src/App.jsx
-import Navbar from './components/navbar/Navbar';
-import Hero from './components/Hero/Hero';
-import Footer from './components/footer/Footer' ;
-import Contact from './components/Contact/Contact' ;
-
+import Navbar from "./components/navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/footer/Footer";
+import Cjem from "./components/Section1/Cjem";
+import Jeea from "./components/section2/Jeea";
 
 function App() {
-   useEffect(()=>{
+  useEffect(() => {
     AOS.init();
-   },[])
+  }, []);
 
   return (
-<<<<<<< HEAD:frontend/src/App.js
     <div className="App">
       <Navbar />
       <Hero />
-=======
-    <div>
-      <h1>
-        JEEA
-      </h1>
-      <JeeaPresentation/>
->>>>>>> 369c28e2051fa4540339639b789099704c8ee4b3:frontend/src/App.jsx
+      <Cjem />
+      <Jeea />
+      <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/section2/Jeea.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import "./jeea.scss"
+
+//assets 
+import jeea from "../../assets/jeea.svg"
+import star from "../../assets/star.svg"
+
+const Jeea = () => {
+  return (
+    <section id="JEEA" className='jeea'>
+        <div className='jeea__container'>
+       <div className='title__container'>
+        <h1 className='title'>WHO ARE WE?</h1>
+        
+        <img src={star} alt="star" className='starImg'/> 
+       
+       </div>
+       <div className='content'>
+        <p className='paragraph'>
+        CJEM (Confédération des Juniors Entreprises Marocaines) est la structure fédératrice du mouvement Junior-Entrepreneuriat à l'échelle marocaine dans le but de promouvoir le concept des JEs (Junior Enterprises) au niveau national, mais aussi faire en sorte que le statut du JE soit juridiquement reconnu et valorisé auprès des entreprises.
+Pour plus d'informations : https://cjem.ma/
+        </p>
+
+        <img src={jeea} alt="img" className='jeeaImg'/>
+       </div>
+
+       </div>
+        </section>
+  )
+}
+
+export default Jeea
