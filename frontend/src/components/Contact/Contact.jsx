9a3de863d5210e2a2da0e1886e661df97ef1f427
hamza-ihind <ihind.mizo@gmail.com>
2023-02-07 02:23:12 +0100

    made some final changes on overall design

--- frontend/src/components/Contact/Contact.jsx
+++ frontend/src/components/Contact/Contact.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import "./Contact.scss";
-<<<<<<< HEAD
+import star from "../../assets/star.svg";
 import emailjs from "@emailjs/browser";
 import { useRef } from "react";
 
-=======
-import star from "../../assets/star.svg";
->>>>>>> e0cc93efc6916af50395e92bbb43ad30f897cf3f
 const Footer = () => {
   const sendEmail = (e) => {
     e.preventDefault();
@@ -32,97 +29,92 @@ const Footer = () => {
   return (
     <div className="container">
       <div className="content">
-          <div className="contact-text">
-              <div className="title__container">
-                <h1 className="title">CONTACT US</h1>
-                <img src={star} alt="star" className="starImg" />
-              </div>
-              <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum,
-<<<<<<< HEAD
-              sed, iusto harum et officiis
-            </p>
-=======
-              sed, iusto harum et officiis{" "}
-              </p>
->>>>>>> e0cc93efc6916af50395e92bbb43ad30f897cf3f
+        <div className="contact-text">
+          <div className="title__container">
+            <h1 className="title">CONTACT US</h1>
+            <img src={star} alt="star" className="starImg" />
           </div>
-          <form
-            ref={form}
-            onSubmit={sendEmail}
-            method="POST"
-            id="contactForm"
-            name="contactForm"
-            class="contactForm"
-          >
-            <div>
-              <div className="Name_Area input">
-                <div className="name_input">
-                  {" "}
-                  <input
-                    type="text"
-                    class="form-control"
-                    name="Firstname"
-                    id="Firstname"
-                    placeholder="Your Firstname"
-                  />{" "}
-                </div>
-                <div className="name_input">
-                  {" "}
-                  <input
-                    type="text"
-                    class="form-control"
-                    name="Lastname"
-                    id="Lastname"
-                    placeholder="Your Lastname"
-                  />{" "}
-                </div>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum,
+            sed, iusto harum et officiis
+          </p>
+        </div>
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          method="POST"
+          id="contactForm"
+          name="contactForm"
+          class="contactForm"
+        >
+          <div>
+            <div className="Name_Area input">
+              <div className="name_input">
+                {" "}
+                <input
+                  type="text"
+                  class="form-control"
+                  name="Firstname"
+                  id="Firstname"
+                  placeholder="Your Firstname"
+                />{" "}
               </div>
-
-              <div className="input">
-                <div>
-                  {" "}
-                  <input
-                    type="email"
-                    class="form-control"
-                    name="email"
-                    id="email"
-                    placeholder="Email"
-                  />{" "}
-                </div>
-              </div>
-
-              <div className="input">
-                <div>
-                  {" "}
-                  <input
-                    type="text"
-                    class="form-control"
-                    name="subject"
-                    id="subject"
-                    placeholder="Subject"
-                  />{" "}
-                </div>
-              </div>
-
-              <div className="input ">
-                <div>
-                  <textarea
-                    name="message"
-                    class="form-control"
-                    id="message"
-                    cols="30"
-                    rows="4"
-                    placeholder="Your message"
-                  ></textarea>
-                </div>
-              </div>
-
-              <div className="submit_btn">
-                <input type="submit" value="SUBMIT" />
+              <div className="name_input">
+                {" "}
+                <input
+                  type="text"
+                  class="form-control"
+                  name="Lastname"
+                  id="Lastname"
+                  placeholder="Your Lastname"
+                />{" "}
               </div>
             </div>
-          </form>
+
+            <div className="input">
+              <div>
+                {" "}
+                <input
+                  type="email"
+                  class="form-control"
+                  name="email"
+                  id="email"
+                  placeholder="Email"
+                />{" "}
+              </div>
+            </div>
+
+            <div className="input">
+              <div>
+                {" "}
+                <input
+                  type="text"
+                  class="form-control"
+                  name="subject"
+                  id="subject"
+                  placeholder="Subject"
+                />{" "}
+              </div>
+            </div>
+
+            <div className="input ">
+              <div>
+                <textarea
+                  name="message"
+                  class="form-control"
+                  id="message"
+                  cols="30"
+                  rows="4"
+                  placeholder="Your message"
+                ></textarea>
+              </div>
+            </div>
+
+            <div className="submit_btn">
+              <input type="submit" value="SUBMIT" />
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   );
--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,55 +1,77 @@
 import React from "react";
-import "./Footer.scss" ;
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./Footer.scss";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Footer = () => {
   return (
     <div className="footer-clean">
-        <div className="content ">
-
+      <div className="content ">
         <div className="content_flex">
-            <div className="left_side" >
-
-                <div className="footer_nav top">
-                    <ul>
-                       <li><a href="#" className="active">Home</a></li>
-                        <li><a href="#">Services</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Team</a></li>
-                    </ul>
-                </div>
-
-                <div className="middle">
-                   <div className="logo">
-                    <h1>JEEA</h1>
-                   </div>
-                   <div class=" item_social">
-                       <a href="#"><i class="fa fa-facebook"></i></a>
-                       <a href="#"><i className="fa fa-twitter "></i></a>
-                       <a href=""><i className="fa fa-linkedin"></i></a>
-                       <a href="#"><i className="fa fa-instagram"></i></a>
-                   </div>
-                </div>
-                
-                <div className="footer_nav buttom">
-                    <ul>
-                       <li><a href="#">Terms of service</a></li>
-                        <li><a href="#">Contact</a></li>
-                        <li><a href="#">Report</a></li>
-                    </ul>
-                </div>
-              
+          <div className="left_side">
+            <div className="footer_nav top">
+              <ul>
+                <li>
+                  <a href="#" className="active">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a href="#">Services</a>
+                </li>
+                <li>
+                  <a href="#">About</a>
+                </li>
+                <li>
+                  <a href="#">Team</a>
+                </li>
+              </ul>
             </div>
 
-            <div className="right_side " >
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting . </p>
+            <div className="middle">
+              <div className="logo">
+                <h1>JEEA</h1>
+              </div>
+              <div class=" item_social">
+                <a href="#">
+                  <i class="fa fa-facebook"></i>
+                </a>
+                <a href="#">
+                  <i className="fa fa-twitter "></i>
+                </a>
+                <a href="">
+                  <i className="fa fa-linkedin"></i>
+                </a>
+                <a href="#">
+                  <i className="fa fa-instagram"></i>
+                </a>
+              </div>
+            </div>
+
+            <div className="footer_nav buttom">
+              <ul>
+                <li>
+                  <a href="#">Terms of service</a>
+                </li>
+                <li>
+                  <a href="#">Contact</a>
+                </li>
+                <li>
+                  <a href="#">Report</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="right_side ">
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting .{" "}
+            </p>
             <span>contact@JEEA.com</span>
-            <span>(+212) 645-987-756</span>   
-            </div>
-
+            <span>(+212) 645-987-756</span>
+          </div>
         </div>
-
-        </div>
-   <p className="copyright">Copyright © JEEA | ENSA AGADIR </p>
-</div>);
+      </div>
+      <p className="copyright">Copyright © JEEA | ENSA AGADIR </p>
+    </div>
+  );
 };
 export default Footer;
