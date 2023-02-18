2fa30c81d5a9b1657780325c8070e68e26a196de
hamza-ihind <ihind.mizo@gmail.com>
2023-02-18 23:05:11 +0100

    changes

--- frontend/src/database/Members.js
+++ frontend/src/database/Members.js
@@ -2,57 +2,59 @@
 import hamza from "../assets/Members/ihind.jpg";
 import aqnouch from "../assets/Members/aqnouch.jpeg";
 import zouane from "../assets/Members/zouane.jpg";
+import errai from "../assets/Members/errai.jpg";
+import tariq from "../assets/Members/tariq.jpeg";
 
 // Databases
 export const info = [
   {
-    name: "Hamza ihind",
+    name: "Tariq AIT LAKHLIFT",
+    img: tariq,
+  },
+  {
+    name: "Mohamed MOULAY",
     img: hamza,
   },
   {
-    name: "Hamza ihind",
+    name: "Mohamed Amine JAAFOURI",
     img: hamza,
   },
   {
-    name: "Hamza ihind",
-    img: hamza,
-  },
-  {
-    name: "Hamza ihind",
-    img: hamza,
-  },
-  {
-    name: "Hamza ihind",
-    img: hamza,
-  },
-  {
-    name: "Hamza ihind",
+    name: "Abdelhak MEKAOUI",
     img: hamza,
   },
 ];
 
 export const design = [
   {
-    name: "Hassan Elqadi",
-    img: aqnouch,
+    name: "Hassan ELQADI",
+    // img: hassan,
+  },
+  {
+    name: "Zakaria Ahrbil",
+    // img: ahrbil,
+  },
+  {
+    name: "Manal ELMOUSSAOUI",
+    // img: hassan,
   },
 ];
 
 export const rtc = [
   {
-    name: "Oumaima Zouane",
+    name: "Oumaima ZOUANE",
     img: zouane,
   },
   {
-    name: "Hassan Elqadi",
+    name: "Samira ERRAI",
+    img: errai,
+  },
+  {
+    name: "Sara AQNOUCH",
     img: aqnouch,
   },
   {
-    name: "Hassan Elqadi",
-    img: aqnouch,
-  },
-  {
-    name: "Hamza ihind",
+    name: "Amine AIT HAMMA",
     img: hamza,
   },
 ];
